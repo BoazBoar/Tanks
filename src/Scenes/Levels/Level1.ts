fe5dc6eecda06eec49784626ce4fdf8970aaae16
BoazVaneveld <boazvaneveld@outlook.com>
2025-02-14 12:53:31 +0100

    Added images for Level1

--- src/Scenes/Levels/Level1.ts
+++ src/Scenes/Levels/Level1.ts
@@ -3,5 +3,8 @@ import Level from './Level';
 export default class Level1 extends Level {
   public constructor(maxX: number, maxY: number) {
     super(maxX, maxY);
+
+    this.levelMapBackground.src = 'assets/LevelBackgrounds/tanksLevel1Background.png';
+    this.levelMapForeground.src = 'assets/LevelForegrounds/tanksLevel1Foreground.png';
   }
 }
